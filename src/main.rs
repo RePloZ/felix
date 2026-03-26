1c12092610914faad8c4bee901054d5f8879b7b1
Hervé Kay <herve.kay@pm.me>
2026-03-26 01:30:53 +0100

    :sparkles: parse correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use tokio::{io::AsyncWriteExt, net::TcpListener};
+
+use bytes::{BufMut, BytesMut};
 
 use crate::request::header::RequestHeader;
 
 pub mod request;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buffer = [0; 1024];
-                let _ = stream.read(&mut buffer);
+    let listener = TcpListener::bind("127.0.0.1:9092").await?;
+    loop {
+        match listener.accept().await {
+            Ok((mut stream, _)) => {
+                tokio::spawn(async move {
+                    let mut buf = BytesMut::with_capacity(8);
 
-                let req_header = RequestHeader::from(&buffer);
+                    let req_header = RequestHeader::parse_request(&mut stream).await;
+                    buf.put_i32(0);
+                    buf.put_i32(req_header.correlation_id);
+                    buf.put_i16(req_header.check_version());
 
-                let _ = stream.write_all(&(0i32).to_be_bytes());
-                let _ = stream.write(&req_header.correlation_id.to_be_bytes());
-                let _ = stream.flush();
-
-                println!("accepted new connection");
+                    stream.write_all_buf(&mut buf).await.unwrap()
+                });
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(e) => println!("error: {}", e),
         }
     }
 }
--- src/request/header.rs
+++ src/request/header.rs
@@ -1,22 +1,49 @@
+use thiserror::Error;
+use tokio::{io::AsyncReadExt, net::TcpStream};
+
 pub struct RequestHeader {
     pub api_key: i16,
     pub api_version: i16,
     pub correlation_id: i32,
+    pub msg_size: i32,
     pub client_id: Option<Vec<u8>>,
     pub tag_buffer: Option<Vec<u8>>,
 }
 
-impl From<&[u8; 1024]> for RequestHeader {
-    fn from(data: &[u8; 1024]) -> Self {
-        let client_id = data[8..].to_vec();
+#[derive(Debug, Error)]
+pub enum HeaderError {
+    #[error("the data for key `{0}` is not available")]
+    ReadElement(#[from] tokio::io::Error),
+}
 
-        let tag_buffer = None;
+impl From<(i32, i16, i16, i32)> for RequestHeader {
+    fn from(header: (i32, i16, i16, i32)) -> Self {
+        let (msg_size, api_key, api_version, correlation_id) = header;
         Self {
-            api_key: i16::from_be_bytes([data[0], data[1]]),
-            api_version: i16::from_be_bytes([data[2], data[3]]),
-            correlation_id: i32::from_be_bytes([data[8], data[9], data[10], data[11]]),
-            client_id: Some(client_id),
-            tag_buffer: tag_buffer,
+            msg_size,
+            api_key,
+            api_version,
+            correlation_id,
+            client_id: None,
+            tag_buffer: None,
+        }
+    }
+}
+
+impl RequestHeader {
+    pub async fn parse_request(stream: &mut TcpStream) -> Self {
+        let msg_size = stream.read_i32().await.unwrap();
+        let api_key = stream.read_i16().await.unwrap();
+        let api_ver = stream.read_i16().await.unwrap();
+        let correlation_id = stream.read_i32().await.unwrap();
+
+        Self::from((msg_size, api_key, api_ver, correlation_id))
+    }
+
+    pub fn check_version(&self) -> i16 {
+        match self.api_version {
+            0..=4 => 0,
+            _ => 35,
         }
     }
 }
